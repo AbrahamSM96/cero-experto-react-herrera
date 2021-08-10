979444a1c16e0c53894d01bec49285fe024257f8
Abraham Serrano <abrahamserrano96@hotmail.com>
2021-08-10 12:17:01 -0500

    Add Array destructuring in 02-intro-javascript

---
+++ 02-intro-javascript/src/js-fundamentals/07-destructuringArrays.js
@@ -0,0 +1,34 @@
+//Destructuracion de arreglos
+
+const characters = ["Kaiu", "King Kong", "Godzilla"];
+// Si solamente quiero el segundo elemento del array debo de poner una coma para indicarle al array que no
+//me interesa la posicion uno
+const [, , p3] = characters;
+
+console.log(p3);
+
+const returArray = () => {
+    return ["ABC", 124];
+};
+
+const [letras, numeros] = returArray();
+
+console.log(letras, numeros);
+
+//Tarea
+const useState = (value) => {
+    return [
+        value,
+        () => {
+            console.log("Hola mundoooo d");
+        },
+    ];
+};
+
+const [nombre, setNombre] = useState("King Kong");
+// console.log(arr, "ARR");
+
+// arr[1]();
+
+console.log(nombre);
+setNombre();
