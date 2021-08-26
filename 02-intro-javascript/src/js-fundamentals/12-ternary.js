0a01d3a39604aa2f1058f2afa3486957be714fa2
Abraham Serrano <abrahamserrano96@hotmail.com>
2021-08-26 18:12:41 -0500

    add 12-ternary

---
+++ 02-intro-javascript/src/js-fundamentals/12-ternary.js
@@ -0,0 +1,15 @@
+const active = true;
+
+// let mensaje = "";
+
+// if (active) {
+//     mensaje= 'Activo'
+// } else{
+//     mensaje = 'Inactivo'
+// }
+
+// const mensaje = active ? "Activo" : "Inactivo";
+
+const mensaje = active && "Activo";
+
+console.log(mensaje);
