54517042f9327129f2dfc44f7853e8891141d399
Abraham Serrano <abrahamserrano96@hotmail.com>
2021-08-29 18:55:55 -0500

    create a component in 03-counter-app

---
+++ 03-counter-app/src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import PrimeraApp from "./PrimeraApp";
+import "./css/index.css";
+const saludo = <h1>Hola Mundo</h1>;
+
+const root = document.querySelector("#root");
+ReactDOM.render(<PrimeraApp />, root);
