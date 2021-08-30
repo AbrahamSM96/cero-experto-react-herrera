c77e52162739d5bc6a8d90a2afb2cd8119046bc9
Abraham Serrano <abrahamserrano96@hotmail.com>
2021-08-29 20:08:00 -0500

    add proptypes

--- 03-counter-app/src/PrimeraApp.js
+++ 03-counter-app/src/PrimeraApp.js
@@ -1,12 +1,14 @@
 import React from "react";
+import PropTypes from "prop-types";
 //Functional Components
-const PrimeraApp = ({ saludos = "Hola lalala " }) => {
+const PrimeraApp = ({ saludos }) => {
     const saludo = "Hola perros";
     const saludo2 = [1, 2, 3, 4, 5, 6];
     const saludo3 = {
         name: "Abraham",
         edad: "25",
     };
+
     return (
         <>
             {/* <h1>{saludo}</h1>
@@ -15,6 +17,10 @@ const PrimeraApp = ({ saludos = "Hola lalala " }) => {
             <p>{saludos}</p>
         </>
     );
+};
+
+PrimeraApp.propTypes = {
+    saludos: PropTypes.string.isRequired,
 };
 
 export default PrimeraApp;
