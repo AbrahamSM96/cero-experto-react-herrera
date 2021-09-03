f46710641c4122c11a46bc8510e4e43a924bddcb
Abraham Serrano <abrahamserrano96@hotmail.com>
2021-09-02 20:10:05 -0500

    add use state and handle click

--- 03-counter-app/src/CounterApp.js
+++ 03-counter-app/src/CounterApp.js
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
-export default function CounterApp({ value }) {
-    const handleClick = (e) => {
-        console.log(e);
+export default function CounterApp({ value = 0  }) {
+    const [counter, setCounter] = useState(value);
+
+    const handleClickPlus = (e) => {
+        setCounter(counter + 1);
+        // setCounter((prev) => prev + 1);
     };
+    const handleClickSubstract = (e) => setCounter(counter - 1);
+
+    const handleClickReset = (e) => setCounter(value);
+
     return (
         <>
             <h1>CounterApp</h1>
-            <h1>{value}</h1>
-            <button onClick={handleClick}>+1</button>
+            <h1>{counter}</h1>
+            <button onClick={handleClickPlus}>+1</button>
+            <button onClick={handleClickReset}>Reset</button>
+            <button onClick={handleClickSubstract}>-1</button>
         </>
     );
 }
--- 03-counter-app/src/index.js
+++ 03-counter-app/src/index.js
@@ -4,4 +4,4 @@ import ReactDOM from "react-dom";
 import CounterApp from "./CounterApp";
 import "./css/index.css";
 const root = document.querySelector("#root");
-ReactDOM.render(<CounterApp value={12345} />, root);
+ReactDOM.render(<CounterApp value={0} />, root);
